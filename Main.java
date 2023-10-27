12bc826fdf0b1f63082fbcf3808e7ec7d11cda68
User <yulia072003@mail.ru>
2023-10-27 08:40:51 +0300

    div changes 3

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке div");
         System.out.print("Это добавлено в ветке div ещё раз");
+        System.out.print("Это добавлено в ветке div и ещё раз");
     }
 }
